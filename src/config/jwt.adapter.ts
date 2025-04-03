7536d912145943b1979fd0d41675bd02fddcf9b6
davidperezboza <david.perez@babelgroup.com>
2025-04-03 10:50:53 +0200

    Video 273

--- src/config/jwt.adapter.ts
+++ src/config/jwt.adapter.ts
@@ -1,9 +1,12 @@
 import jwt from 'jsonwebtoken';
+import { envs } from './envs';
+
+const JWT_SEED = envs.JWT_SEED;
 
 export class JwtAdapter{
     static generateToken = async (payload: any, duration: string = '2h') => {
         return new Promise((resolve) => {
-            jwt.sign(payload, "SEED", { expiresIn: duration as jwt.SignOptions['expiresIn'] }, (err, token) => {
+            jwt.sign(payload, JWT_SEED, { expiresIn: duration as jwt.SignOptions['expiresIn'] }, (err, token) => {
         
                 if ( err ) return resolve(null);
         
