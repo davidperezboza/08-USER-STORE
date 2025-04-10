19c540401e3c8b1c4303e34fb58fdbb8478bbe98
davidperezboza <david.perez@babelgroup.com>
2025-04-10 19:01:17 +0200

    Video 306 V2

--- src/domain/dtos/products/create-product.dto.ts
+++ src/domain/dtos/products/create-product.dto.ts
@@ -8,4 +8,19 @@ export class CreateProductDto {
         public readonly category: string,
     ){
     };
+
+    static create = (object: { [key: string]: unknown }): [string?, CreateProductDto?] => {
+        const { name, available, price, description, user, category } = object;
+    
+        if (typeof name !== 'string') return ['Invalid or missing name'];
+        if (typeof user !== 'string') return ['Invalid or missing user'];
+        if (typeof category !== 'string') return ['Invalid or missing category'];
+    
+        // Para los opcionales, también puedes validar si quieres
+        const validAvailable = typeof available === 'boolean' ? available : false;
+        const validPrice = typeof price === 'number' ? price : 0;
+        const validDescription = typeof description === 'string' ? description : '';
+    
+        return [undefined, new CreateProductDto(name, validAvailable, validPrice, validDescription, user, category)];
+    };
 };
