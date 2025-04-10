ac650e97cba58d4575c3748eb133f554b5d3e867
davidperezboza <david.perez@babelgroup.com>
2025-04-10 14:53:21 +0200

    Video 306 V1

---
+++ src/domain/dtos/products/create-product.dto.ts
@@ -0,0 +1,11 @@
+export class CreateProductDto {
+    private constructor(
+        public readonly name: string,
+        public readonly available: boolean,
+        public readonly price: number,
+        public readonly description: string,
+        public readonly user: string,
+        public readonly category: string,
+    ){
+    };
+};
