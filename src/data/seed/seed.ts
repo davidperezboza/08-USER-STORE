cad173cea134b7a8c3f93b127f1f32e04be97032
davidperezboza <david.perez@babelgroup.com>
2025-04-14 11:00:39 +0200

    Video 311

--- src/data/seed/seed.ts
+++ src/data/seed/seed.ts
@@ -1,5 +1,7 @@
 import { envs } from '../../config';
-import { MongoDatabase } from '../mongo/mongo-database';
+import { CategoryModel, MongoDatabase, ProductModel, UserModel } from '../mongo';
+import { seedData } from './data';
+
 
 (async() => {
     await MongoDatabase.connect({
@@ -8,8 +10,40 @@ import { MongoDatabase } from '../mongo/mongo-database';
     });
 
     await main();
-});
 
+    await MongoDatabase.disconnect();
+
+    console.log('SEEDED');
+})();
+
+const randomBeetween0AndX = (x: number) => {
+    return Math.floor(Math.random() * x);
+}
 async function main(){
+    await Promise.all([
+        UserModel.deleteMany(),
+        CategoryModel.deleteMany(),
+        ProductModel.deleteMany(),
+    ]);
 
+    const users = await UserModel.insertMany(seedData.users);
+
+    const categories = await CategoryModel.insertMany(
+        seedData.categories.map(category => {
+            return {
+                ...category, 
+                user: users[0]._id
+            };
+        }),
+    );
+
+    const product = await ProductModel.insertMany(
+        seedData.products.map(product => {
+            return {
+                ...product,
+                user: users[randomBeetween0AndX(seedData.users.length - 1)]._id,
+                category: categories[randomBeetween0AndX(seedData.categories.length - 1)]._id,
+            };
+        })
+    );
 };
