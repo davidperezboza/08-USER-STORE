1434b1d0b1c483b4c9b63375e2feec55cd65fdf4
davidperezboza <david.perez@babelgroup.com>
2025-04-14 20:19:41 +0200

    Video 316 V2

--- src/presentation/file-upload/routes.ts
+++ src/presentation/file-upload/routes.ts
@@ -1,12 +1,12 @@
 import { Router } from 'express';
 import { AuthMiddleware } from '../middlewares/auth.middelware';
+import { FileUploadContoller } from '../category/controller';
 
 export class FileUploadRoutes {
     static get routes(): Router {
 
     const router = Router();
-    const categoryService = new CategoryService();
-    const controller = new CategoryContoller(categoryService);
+    const controller = new FileUploadContoller;
   
     router.get('/', controller.getCategory);
     router.post('/', [AuthMiddleware.validateJWT], controller.createCategory);
